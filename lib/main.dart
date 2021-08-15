5206c31a1553b943c4e2e9679a9d81039b91278c
Omar Mohamed <75180396+omardavinci10@users.noreply.github.com>
2021-08-15 16:14:33 +0200

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+  void playSound(int noteNumber) {
+    final player = AudioCache();
+    player.play('note$noteNumber.wav');
+  }
+
+  Widget buildButton(int buttonNumber, Color buttonColor) {
+    return Expanded(
+      child: TextButton(
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all<Color>(buttonColor),
+          ),
+          onPressed: () {
+            playSound(buttonNumber);
+          }),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildButton(1, Colors.red),
+              buildButton(2, Colors.orange),
+              buildButton(3, Colors.yellow),
+              buildButton(4, Colors.green),
+              buildButton(5, Colors.teal),
+              buildButton(6, Colors.blue),
+              buildButton(7, Colors.purple),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
